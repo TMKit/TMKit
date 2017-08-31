75c260d9ac28252416419f2b35ced8ee21edf587
neng18 <cjianneng@outlook.com>
2017-08-31 14:58:48 +0800

    discarded log api
    Signed-off-by: neng18 <cjianneng@outlook.com>

--- Classes/Log/TMLogUtil.m
+++ Classes/Log/TMLogUtil.m
@@ -11,32 +11,32 @@
 
 @implementation TMLogUtil
 
-+ (void)startLog:(NSString*)string
-{
-    NSLog(@"************************** Start %@ **************************", string);
-}
+//+ (void)startLog:(NSString*)string
+//{
+//    NSLog(@"************************** Start %@ **************************", string);
+//}
+//
+//+ (void)stopLog:(NSString*)string
+//{
+//    NSLog(@"************************** End %@ ****************************", string);
+//}
 
-+ (void)stopLog:(NSString*)string
-{
-    NSLog(@"************************** End %@ ****************************", string);
-}
-
-+ (void)output:(const char*)fileName lineNumber:(int)lineNumber function:(const char*)function input:(NSString*)input, ...
-{
-    va_list argList;
-    NSString *filePath, *formatStr;
-    
-    // Build the path string
-    filePath = [[NSString alloc] initWithBytes:fileName length:strlen(fileName)
-                                      encoding:NSUTF8StringEncoding];
-    // Process arguments, resulting in a format string
-    va_start(argList, input);
-    formatStr = [[NSString alloc] initWithFormat:input arguments:argList];
-    va_end(argList);
-    
-    NSLog(@"%@[%d] %s %@", [filePath lastPathComponent], lineNumber, function, formatStr);
-    
-}
+//+ (void)output:(const char*)fileName lineNumber:(int)lineNumber function:(const char*)function input:(NSString*)input, ...
+//{
+//    va_list argList;
+//    NSString *filePath, *formatStr;
+//    
+//    // Build the path string
+//    filePath = [[NSString alloc] initWithBytes:fileName length:strlen(fileName)
+//                                      encoding:NSUTF8StringEncoding];
+//    // Process arguments, resulting in a format string
+//    va_start(argList, input);
+//    formatStr = [[NSString alloc] initWithFormat:input arguments:argList];
+//    va_end(argList);
+//    
+//    NSLog(@"%@[%d] %s %@", [filePath lastPathComponent], lineNumber, function, formatStr);
+//    
+//}
 
 + (void)output:(const char*)fileName lineNumber:(int)lineNumber input:(NSString*)input, ...
 {
