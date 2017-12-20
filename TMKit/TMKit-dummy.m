92f1df93c3d9440dcb598025d424ea8355a40266
Teemo <cjianneng@outlook.com>
2017-12-20 15:08:12 +0800

    update header
    Signed-off-by: Teemo <cjianneng@outlook.com>

---
+++ TMKit/TMKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TMKit : NSObject
+@end
+@implementation PodsDummy_TMKit
+@end
---
+++ TMKit/TMKit-umbrella.h
@@ -0,0 +1,34 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSData+TMEncrypt.h"
+#import "NSData+TMUtil.h"
+#import "NSDate+TMUtil.h"
+#import "TMCaching.h"
+#import "TMDiskCache.h"
+#import "TMUserDefaults.h"
+#import "TMLog.h"
+#import "TMMacroDefine.h"
+#import "NSObject+TMDataValid.h"
+#import "NSString+TMEncrypt.h"
+#import "NSString+TMUtil.h"
+#import "TMOperationGroup.h"
+#import "TMOperationQueue.h"
+#import "TMKit.h"
+#import "TMOperationTypes.h"
+#import "UIColor+TMUtil.h"
+#import "UIView+TMUtil.h"
+#import "TMVersionUtil.h"
+
+FOUNDATION_EXPORT double TMKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char TMKitVersionString[];
+
